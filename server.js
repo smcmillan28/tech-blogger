0368c15a9cfa6f29d8267294066b1b75dddedeb9
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-03-27 21:38:02 -0400

    testing heroku deployment

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-// const mysql = require('mysql');
+const mysql = require('mysql');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const connection = mysql.createConnection({
-//   host: 'us-cdbr-east-03.cleardb.com',
-//   user: 'b555b82140c4c7',
-//   password: '2cefa23e',
-//   database: 'heroku_fcc684b2ff11741'
-// });
+const connection = mysql.createConnection({
+  host: 'us-cdbr-east-03.cleardb.com',
+  user: 'b555b82140c4c7',
+  password: '2cefa23e',
+  database: 'heroku_fcc684b2ff11741'
+});
 
 // connection.connect();
 
@@ -46,6 +46,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening  on http://localhost:${PORT}`));
-});
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log(`Now listening  on http://localhost:${PORT}`));
+// });
+
+connection.connect();
